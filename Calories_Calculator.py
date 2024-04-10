619cd2174fbe2182a3da9c11f7bb90fa2a28baea
xobi_van <xobi_van@Linux.myguest.virtualbox.org>
2024-04-10 20:16:33 +0100

    Updated Calories_Calculator.py

--- Calories_Calculator.py
+++ Calories_Calculator.py
@@ -38,15 +38,15 @@ class CalorieCounterApp(QMainWindow):
         layout.addLayout(view_foods_group)
 
         # Создаем статусную строку
-        self.statusBar().showMessage('Готово')
+        self.statusBar().showMessage('Ready')
 
     def create_menu(self):
         main_menu = self.menuBar()
-        file_menu = main_menu.addMenu('Файл')
+        file_menu = main_menu.addMenu('File')
 
-        exit_action = QAction(QIcon('exit.png'), 'Выход', self)
+        exit_action = QAction(QIcon('exit.png'), 'Exit', self)
         exit_action.setShortcut('Ctrl+Q')
-        exit_action.setStatusTip('Выход из приложения')
+        exit_action.setStatusTip('Quit the app')
         exit_action.triggered.connect(self.close)
 
         file_menu.addAction(exit_action)
@@ -54,25 +54,25 @@ class CalorieCounterApp(QMainWindow):
     def create_add_food_group(self):
         add_food_group = QVBoxLayout()
 
-        label = QLabel("Добавить продукт")
-        label.setFont(QFont("Arial", 16, QFont.Bold))
+        label = QLabel("Add Product")
+        # label.setFont(QFont("Arial", 16, QFont.Bold))
         add_food_group.addWidget(label)
 
         name_layout = QHBoxLayout()
-        name_label = QLabel("Название:")
+        name_label = QLabel("Product:")
         self.name_input = QLineEdit()
         name_layout.addWidget(name_label)
         name_layout.addWidget(self.name_input)
         add_food_group.addLayout(name_layout)
 
         calories_layout = QHBoxLayout()
-        calories_label = QLabel("Калории:")
+        calories_label = QLabel("Calories:")
         self.calories_input = QLineEdit()
         calories_layout.addWidget(calories_label)
         calories_layout.addWidget(self.calories_input)
         add_food_group.addLayout(calories_layout)
 
-        add_button = QPushButton("Добавить")
+        add_button = QPushButton("Add")
         add_button.clicked.connect(self.add_food)
         add_food_group.addWidget(add_button)
 
@@ -81,13 +81,13 @@ class CalorieCounterApp(QMainWindow):
     def create_view_foods_group(self):
         view_foods_group = QVBoxLayout()
 
-        label = QLabel("Добавленные продукты")
-        label.setFont(QFont("Arial", 16, QFont.Bold))
+        label = QLabel("Added Products")
+        # label.setFont(QFont("Arial", 16, QFont.Bold))
         view_foods_group.addWidget(label)
 
         self.table_widget = QTableWidget()
         self.table_widget.setColumnCount(2)
-        self.table_widget.setHorizontalHeaderLabels(["Название", "Калории"])
+        self.table_widget.setHorizontalHeaderLabels(["Name", "Calories"])
         view_foods_group.addWidget(self.table_widget)
 
         return view_foods_group
@@ -100,13 +100,13 @@ class CalorieCounterApp(QMainWindow):
             row_position = self.table_widget.rowCount()
             self.table_widget.insertRow(row_position)
             self.table_widget.setItem(row_position, 0, QTableWidgetItem(name))
-            self.table_widget.setItem(row_position, 1, QTableWidgetItem(calories))
+            self.table_widget.setItem(row_position, 1, QTableWidgetItem(calories + " kcal"))
 
             self.name_input.clear()
             self.calories_input.clear()
 
             total_calories = sum(int(self.table_widget.item(row, 1).text()) for row in range(self.table_widget.rowCount()))
-            self.statusBar().showMessage(f'Общее количество калорий: {total_calories}')
+            self.statusBar().showMessage(f'Total amount of calories: {total_calories} kcal')
 
 
 if __name__ == '__main__':
